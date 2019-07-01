38ea3f82d5ef1a54504d73eb319aedae43859c89
chiaoxk <chiaoxk@github.com>
2019-07-01 15:56:28 +0800

    clear screen

---
+++ Assets/Scripts/Editor/MPipelineAsset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+[ExecuteInEditMode]
+public class MPipelineAsset : RenderPipelineAsset
+{
+
+    private Color clearColor=Color.gray;
+
+    [UnityEditor.MenuItem("SRP-Demo/Create Asset Pipeline")]
+    static void CreateAssetPipeline()
+    {
+        var instance = ScriptableObject.CreateInstance<MPipelineAsset>();
+        UnityEditor.AssetDatabase.CreateAsset(instance, "Assets/SRP-Demo/MPipelineAsset.asset");
+    }
+
+
+    protected override IRenderPipeline InternalCreatePipeline()
+    {
+        return new PipelineInstance(clearColor);
+    }
+}
---
+++ Assets/Scripts/PipelineInstance.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+[ExecuteInEditMode]
+public class PipelineInstance : RenderPipeline
+{
+
+    CommandBuffer cmd = null;
+    private Color clearColor = Color.black;
+
+    public PipelineInstance(Color clearColor)
+    {
+        Debug.Log(clearColor);
+        this.clearColor = clearColor;
+    }
+    public override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
+    {
+        base.Render(renderContext, cameras);
+        cmd = new CommandBuffer();
+        cmd.ClearRenderTarget(true, true, clearColor);
+
+        renderContext.ExecuteCommandBuffer(cmd);
+        cmd.Release();
+        renderContext.Submit();
+    }
+}
